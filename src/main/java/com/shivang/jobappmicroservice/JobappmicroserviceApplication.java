1305d506176502a7a291674af146e880ce9a959c
Shivang Dholaria <dholariashivang@gmail.com>
2024-12-28 17:23:28 -0500

    Initial commit

---
+++ src/main/java/com/shivang/jobappmicroservice/JobappmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.shivang.jobappmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobappmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobappmicroserviceApplication.class, args);
+	}
+
+}
